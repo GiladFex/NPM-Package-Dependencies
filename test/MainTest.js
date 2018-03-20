1d0004d30ff7674fb6294e4b07a67d435559530e
Gilad <federgilad@gmail.com>
2018-03-20 22:53:36 +0200

    Add files via upload

---
+++ test/MainTest.js
@@ -0,0 +1,36 @@
+var aJSON = {
+  "name": "a",
+  "version": "2.1.1",
+  "dependencies": {
+    "a_mock": "1.0.5",
+    "a_test": "1.0.9"
+  }};
+
+var reactJSON = {
+  "name": "react",
+  "version": "16.2.0",
+  "dependencies": {
+    "fbjs": "^0.8.16",
+    "loose-envify": "^1.1.0",
+    "object-assign": "^4.1.1",
+    "prop-types": "^15.6.0"
+  }};
+var assert = chai.assert;
+describe("main.js" , function(){
+    describe("GetDependencies() " , function(){
+        it("should find 56 dependencies for the a package;" , function(){
+            GetDependencies(aJSON);
+                  setTimeout(function() {
+                    assert.equal(Nodes.length, 7);
+                  }, 10000)
+        });
+    });
+    describe("GetDependencies() " , function(){
+        it("should find 56 dependencies for the a package;" , function(){
+            GetDependencies(reactJSON);
+                  setTimeout(function() {
+                    assert.equal(Nodes.length, 7);
+                  }, 10000)
+        });
+    });
+});
