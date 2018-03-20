1a9cd08e30fe419550c0a249240d2e3388d6ab84
Gilad <federgilad@gmail.com>
2018-03-20 16:30:34 +0200

    graph shows results nicely now

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 // FLOW OF CONTENT:
 // whenver you enter an input: jquery records a keypress which sends an ajax to get autocomplete suggestions
-// if package exsist with this name- creates graph:
-// graph creation is a recursive function which seeks all dependencies.
-// each dependency is a circles (function createCircles(), which uses image search api to provide pictures for interactiveness)
-// the level of the dependency is written inside the variable "lines", and if a dependency comes more than once it takes note of that
-// in the end, creates a
+// if package exsist with this name- goes to the recursive function, which gets all dependencies and makes a graph out of them.
+// graph is made of nodes and lines, both are arraies of jsons.
+
 
 $(document).ready(function() {
 
+  // VIS JS - Network Chart configurations.
+  // create an array with node for each dependency.
+  var nodes = new vis.DataSet();
+  var lines = new vis.DataSet();
+  var network = null;
+
   // on finish typing (200 ms of nothing) of any search, updates the description. this prevents ajax requests for every keypress.
   var timer = null;
-  $('.PackageName').keydown(function() {
-    clearTimeout(timer);
-    timer = setTimeout(getPackageFromNPM, 200)
+  $('.PackageName').keyup(function(e) {
+
+    // get keycode of current keypress event
+       var code = (e.keyCode || e.which);
+
+       // do nothing if it's an arrow key -- allows graph navigation without losing focus
+       if(code == 37 || code == 38 || code == 39 || code == 40) {
+           return;
+       }
+
+      clearTimeout(timer);
+      timer = setTimeout(getPackageFromNPM, 200);
+
   });
 
   //makes http request to get a json with a lot of keys to a new json with the key of "_id" only
   //note: Any input is alright. it just wouldn't find anything if input is wrong.
   var NPMPackageNames = {};
 
-  //shows description and calls function which creates graph.
+  //shows description and calls the recursive function -- if found.
   function getPackageFromNPM() {
+    resetGraph();
     if ($(".PackageName").val() == "") //empty string
     {
       $(".card-panel").fadeOut(200);
@@ -33,8 +48,7 @@ $(document).ready(function() {
         success: function(result) {
           $(".card-panel").fadeIn(200);
           $(".description").text(result.description);
-          dep = [];
-          con = [];
+          addNode(($(".PackageName").val()), ($(".PackageName").val()) + " latest");
           GetDependencies(result);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -45,18 +59,23 @@ $(document).ready(function() {
     }
   }
 
-  //recursive function which gets ALL dependencies and creates graphs.
-  var dep = [];
-  var con = [];
+  //clears canvas for future use
+  function resetGraph() {
+    nodes.clear();
+    lines.clear();
+  }
 
+  //recursive function which gets ALL dependencies and creates graphs - nodes and lines.
   function GetDependencies(result) {
     if (result.dependencies != null) {
       for (var i = 0; i < Object.keys(result.dependencies).length; i++) {
-        con.push({
-          from: result.name,
-          to: Object.keys(result.dependencies)[i]
-        });
-        if (dep.indexOf(Object.keys(result.dependencies)[i]) == -1) {
+        try {
+          addLine(result.name, Object.keys(result.dependencies)[i]);
+        } catch (e) {
+
+        }
+        try {
+          addNode(Object.keys(result.dependencies)[i], Object.keys(result.dependencies)[i] + " " + Object.values(result.dependencies)[i].slice(1));
           $.ajax({
             url: "https://registry.npmjs.org/" + Object.keys(result.dependencies)[i] + "/" + Object.values(result.dependencies)[i],
             type: 'GET',
@@ -66,96 +85,33 @@ $(document).ready(function() {
               GetDependencies(newResult);
             }
           });
-          dep.push(Object.keys(result.dependencies)[i]);
+        } catch (e) {
+
         }
       }
-    }
-    console.log(dep);
-    console.log(con);
-
-  }
-
-
-  // VIS JS - Network Chart configurations.
-  // create an array with node for each dependency.
-  var nodes;
-  var nodes = null;
-  var lines = null;
-  var network = null;
-
-  // CHART VISUALIZAION //
-
-  function resetAll() {
-    if (network !== null) {
-      network.destroy();
-      network = null;
+    } else {
+      // no dependencies
     }
   }
 
-  // the circles
-  function createCircles() {
-    resetAll();
-    nodes = [{
-        id: "hello",
-        shape: 'circularImage',
-        image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
-        label: "angular!"
-      },
-      {
-        id: 2,
-        shape: 'circularImage',
-        image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
-        label: "react"
-      },
-      {
-        id: 3,
-        shape: 'circularImage',
-        image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
-        label: "a"
-      },
-      {
-        id: 4,
-        shape: 'circularImage',
-        image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
-        label: "b"
-      }
-    ];
+  //adds one line
+  function addLine(from, to) {
+    lines.add({
+      from: from,
+      to: to
+    });
   }
 
-  // gets images off the internet for better visualization
-  // function getImage(PackageName) {
-  //   $.ajax({
-  //     url: "https: //api.qwant.com/api/search/images?count=1&offset=1&q=" + PackageName,
-  //     type: 'GET',
-  //     contentType: "application/json; charset=utf-8",
-  //     dataType: "json",
-  //     success: function(result) {
-  //       return result.result.items.media;
-  //     }
-  //   });
-  // }
+  function addNode(id, label) {
+    nodes.add({
+      id: id,
+      shape: 'circularImage',
+      image: 'https://res.cloudinary.com/snyk/image/upload/v1468845142/logo/snyk-avatar.png', //was about to add custom image with search api but I couldn't find ONE decent service.
+      label: label
+    });
+  }
 
-  // the lines.
-  var lines = [{
-      from: "hello",
-      to: 2
-    },
-    {
-      from: 2,
-      to: 3
-    },
-    {
-      from: 2,
-      to: 4
-    }
-  ];
-
-
-
-  createCircles(); //remove in the end - so far just so i have a chart without doing anything
-
-
-  // create a network -- static data for the chart
+  //  VIS JS STATIC SETTINGS  //
   var container = document.getElementById('mynetwork');
 
   var data = {
